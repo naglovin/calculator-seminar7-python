ced9a7581f0e5b0ada686fa07031f8b6b4cfdc3d
Danil <Kutfilloev.danil@yandex.ru>
2022-10-18 12:17:30 +0500

    добавлен логгер

--- controller.py
+++ controller.py
@@ -1,10 +1,13 @@
 import model_degree as model
 import view
+import logger as log
+
 
 def click():
     num_a = view.get_num()
     num_b = view.get_num()
     model.init(num_a, num_b)
     result = model.do_it()
+    log.record(result)
     view.view_data("Ответ: ",result)
     
--- logger.py
+++ logger.py
@@ -2,8 +2,8 @@ from datetime import datetime as dt
 from time import time
 
 def record (data):
-    time = dt.now().strftime('%H: %M')
-    with open('log.csv', 'a') as file:
-        file.write('{}; действие; {}'
-                    .format(time))
-                    
+    time = dt.now().strftime('%H: %M')              #часы минуты 
+    with open('log.csv', 'a') as file:              #запись в файл, даже если его нет.
+        file.write('{}; time of action; {}\n'              # слева время, time of action справа ответ от действия и переход на новую строку
+                    .format(time, data))
+
